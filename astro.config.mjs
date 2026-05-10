09575e910619ca44af684aeb243925c5ee4cb82d
Murakami1704 <154502901+Murakami1704@users.noreply.github.com>
2026-05-11 03:54:23 +0900

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // あなたのGitHubユーザー名に合わせて変更
-  site: 'https://Murkami1704.github.io',
-  // リポジトリ名が「username.github.io」以外（例：portfolio）なら以下を追加
-  // base: '/portfolio', 
+  site: 'https://Murakami1704.github.io',
+  base: '/portfolioMurakami1704', // これが絶対に必要です！
 });
